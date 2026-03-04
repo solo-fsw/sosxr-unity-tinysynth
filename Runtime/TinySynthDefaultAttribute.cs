c4e32bf45cbfdaed6e8c37f5af6d212331f9b809
mrstruijk <mrstruijk@gmail.com>
2026-03-04 16:03:27 +0100

    removed enhancedlogger depencency

--- Runtime/TinySynthDefaultAttribute.cs
+++ Runtime/TinySynthDefaultAttribute.cs
@@ -1,6 +1,5 @@
 using System;
 
-
 namespace SOSXR.TinySynth
 {
     /// <summary>
@@ -13,11 +12,12 @@ namespace SOSXR.TinySynth
         /// <summary>The default value for the decorated field, in the same unit as the field itself.</summary>
         public readonly float value;
 
-
         /// <summary>Initialises the attribute with the given default value.</summary>
         /// <param name="value">Default value for the decorated field.</param>
         public TinySynthDefaultAttribute(float value)
         {
             this.value = value;
+        }
     }
 }
+
